c4e038cdb3be369dd30dcbc39eabc7b0c412cec4
labadcloyd <abadcloyd@gmail.com>
2021-07-02 20:10:19 +0000

    study on the theory of threadpool

---
+++ threads.js
@@ -0,0 +1,48 @@
+const OS = require('os')
+process.env.UV_THREADPOOL_SIZE = OS.cpus().length
+
+console.log(OS.cpus().length)
+const crypto = require('crypto')
+
+const start = Date.now()
+/* FIRST FOUR */
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('1:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('2:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('3:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('4:', Date.now()-start);
+})
+
+/* SECOND FOUR */
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('5:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('6:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('7:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('8:', Date.now()-start);
+})
+
+/* THIRD FOUR */
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('9:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('10:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('11:', Date.now()-start);
+})
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+	console.log('12:', Date.now()-start);
+})
