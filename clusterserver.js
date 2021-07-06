c7a91ac793ccdb7bf10fea0ef0e668aac52c97c3
labadcloyd <abadcloyd@gmail.com>
2021-07-06 20:10:19 +0000

    setting up simple express server and installed pm2

--- clusterserver.js
+++ clusterserver.js
@@ -1,14 +1,7 @@
-const os = require('os')
-const cluster = require('cluster');
+
 const app = require('express')();
 
-if(cluster.isMaster){
-	const cpunumber = os.cpus().length
-	for(let i = 0; i < cpunumber; i++){
-		cluster.fork()
-		console.log('forked cluster')
-	}
-}else {
+
 	app.get('/',(req,res)=>{
 		res.send('hello world')
 	})
@@ -16,4 +9,3 @@ if(cluster.isMaster){
 	app.listen(3000, ()=>{
 		console.log('server running on port 3000')
 	})
-}
