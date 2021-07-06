f24298b6de6d4a2acb215548ab1f07ad39e72316
labadcloyd <abadcloyd@gmail.com>
2021-07-06 20:10:19 +0000

    added multi cluster

--- clusterserver.js
+++ clusterserver.js
@@ -1,10 +1,19 @@
+const os = require('os')
 const cluster = require('cluster');
 const app = require('express')();
 
-app.get('/',(req,res)=>{
-	console.log()
-})
-
-app.listen(3000, ()=>{
-	console.log('server running on port 3000')
-})
+if(cluster.isMaster){
+	const cpunumber = os.cpus().length
+	for(let i = 0; i < cpunumber; i++){
+		cluster.fork()
+		console.log('forked cluster')
+	}
+}else {
+	app.get('/',(req,res)=>{
+		res.send('hello world')
+	})
+	
+	app.listen(3000, ()=>{
+		console.log('server running on port 3000')
+	})
+}
