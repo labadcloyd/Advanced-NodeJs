1c6c60fa9f008530f2fdecafdcefb0ec62f1da60
labadcloyd <abadcloyd@gmail.com>
2021-08-16 18:57:14 +0800

    setting up redis

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const app = require('express')();
 require('dotenv').config({path:'./config.env'})
-
+const redis = require('redis');
+const client = redis.createClient({
+    host: process.env.REDISHOSTNAME,
+    port: process.env.REDISPORT,
+    password: process.env.REDISPASSWORD
+});
 /* This is when nodejs shouldn't be used:
 	Processes that are cpu intensive will block the server and delay other requests since the event loop is single threaded.
 
@@ -16,4 +21,5 @@ app.get('/', (req,res)=>{
 })
 app.listen(3000,()=>{
 	console.log('running on port 3000')
+	// console.log(client)
 })
